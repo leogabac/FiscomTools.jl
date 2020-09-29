4393ad61e982037b1961b607857a93f25fbf168a
leogabac <71944972+leogabac@users.noreply.github.com>
2020-09-28 21:48:55 -0500

    Update FiscomTools.jl

--- src/FiscomTools.jl
+++ src/FiscomTools.jl
@@ -2,7 +2,7 @@ module FiscomTools
 include("test.jl")
 include("incremental.jl")
 include("bisection.jl")
-include("diff.jl")
+include("numdiff.jl")
 include("newton.jl")
 export bisection,incremental,diff,newtonR
 end

685b74f329942a81fe6f8f753dc0a50578ec2b0c
leogabac <71944972+leogabac@users.noreply.github.com>
2020-10-21 14:07:49 -0500

    Typo fixed

--- src/numdiff.jl
+++ src/numdiff.jl
@@ -7,7 +7,7 @@ function numdiff(f,x::Number,h::Float64)
 end
 
 # Richardson function
-function numdiff(f,x::Number,h::Intt64)
+function numdiff(f,x::Number,h::Int64)
     D0 = (f(x+h)-f(x-h))/(2*h);
     D1 = ( f(x+(h/2)) - f(x-(h/2)) )/(h);
     TV = D1 + (D1-D0)/3

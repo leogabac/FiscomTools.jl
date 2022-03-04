0c396f78c2c303c3b78b027be0e921165f179dee
leogabac <leogabac@gmail.com>
2022-03-04 21:21:49 +0100

    Update trapz.jl
    Removed interpolate.jl

--- src/trapz.jl
+++ src/trapz.jl
@@ -1,28 +1,40 @@
 # Integration using a function
-function trapz(f, a, b, h)
-    x = collect(Float64, a:h:b);
-    suma = 0
-    for j ∈ 2:(length(x) - 1)
-        val = f(x[j])
-        suma += val
-    end
-    I = (b - a) * (f(x[1]) + 2 * suma + f(x[end])) / (2 * (length(x) - 1));
+
+"""
+    trapz(f,a,b; h)
+
+Computes the integral of `f(x)` from `a` to `b` using the trapezoidal rule.
+
+The keyword (optional) argument defines the spacing between points. The default value is `h = 0.01`.
+
+    trapz(x::Vector, y::Vector)
+
+Computes the approximate integral using the discretized 1D mesh `x` and `y` using the trapezoidal rule. It is assumed that `x` is equally spaced.
+
+# Examples
+```julia-repl
+julia> f(x) = cos(x) ;
+julia> trapz(f,0,π/2, h = 1e-5 )
+0.999999999939998
+
+julia> x = collect(Float64,0:1e-5:π/2) ;
+julia> y  = f.(x) ;
+julia> trapz(x,y)
+0.9999999999716525
+```
+"""
+function trapz(f, a::Real, b::Real; h::Float64 = 0.01)
+    Δ = (b-a)
+    mid = sum( f(x) for x in (a+h:h:b-h) )
+    I = Δ * ( f(a) + 2 * mid + f(b) ) / (2 * (Δ/h) );
     return I
 end
 
 
 # Integration of a table
-function trapz(x::Array{Float64,1}, y::Array{Float64,1})
-    suma = 0;
-    for j ∈ 2:(length(x) - 1)
-        suma += y[j]
-    end
-    I = (x[end] - x[1]) * (y[1] + 2 * suma + y[end]) / (2 * (length(x) - 1));
+function trapz(x::Vector, y::Vector)
+    h, Δ = abs(x[2] - x[1]), x[end] - x[1]
+    mid = sum( y[2:end-1] )
+    I = Δ * (y[1] + 2 * mid + y[end]) / (2 * (Δ/h));
     return I
 end
-
-
-
-
-
-

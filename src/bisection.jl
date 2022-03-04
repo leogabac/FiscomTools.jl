5d32b953662e6b031fc041dcef89eb3975ddf9f7
leogabac <leogabac@gmail.com>
2022-03-04 20:13:24 +0100

    Update bisection.jl

--- src/bisection.jl
+++ src/bisection.jl
@@ -1,23 +1,24 @@
 
 
 """
-    bisection(f, (a,b) ;TOL)
+    bisection(f,a,b;TOL)
 
-Approximates the root of `f(x)` in the interval from `a` and `b` using the bisection method.
+Approximates the root of `f(x)` in the interval between `a` and `b` using the bisection method.
+
+The keyword (optional) argument `TOL` refers to the stop criteria. The default value is `TOL = 0.01`
 
 # Examples
 ```julia-repl
 julia> f(x) = x^2 - 1 ;
-julia> bisection(f, (0,3), TOL = 1e-5 )
+julia> bisection(f,0,3, TOL = 1e-5 )
 1.0000019073486328
 
 ```
 
 """
-function bisection(f, ansatz::Tuple{Number,Number}; TOL::Float64=0.01)
-    (lower,upper) = ansatz
+function bisection(f, lower::Real ,upper::Real; TOL::Float64=0.01)
     mid = (lower + upper)/2;
-    ea = 1; #Initialize
+    ε = 1; #Initialize
     
     # We do the first iteration
     if f(lower)*f(mid) < 0
@@ -29,10 +30,10 @@ function bisection(f, ansatz::Tuple{Number,Number}; TOL::Float64=0.01)
     end
 
     #Now we start doing the iterations until it is done
-    while ea > TOL
+    while ε > TOL
         last = mid #We store the last approximation
         mid = (lower + upper)/2
-        ea = abs((mid-last)/mid);
+        ε = abs((mid-last)/mid);
         
         if f(lower)*f(mid) < 0
             upper = mid
@@ -46,3 +47,6 @@ function bisection(f, ansatz::Tuple{Number,Number}; TOL::Float64=0.01)
 
     return mid
 end
+
+f(x) = x^2 - 1;
+@code_warntype bisection(f,0,3, TOL = 1.0e-5)
